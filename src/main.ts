54325b64801007b895a3f457009f0e2f6990323f
Christian Osorio <ccdosorio11@gmail.com>
2023-08-19 00:10:17 -0600

    Fin seccion 4: DTO, Pipes, Validations, POST, PATCH AND DELETE

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Remueve data basura del DTO
+      forbidNonWhitelisted: true, // Para no enviar propiedades que no forman parte del DTO
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
